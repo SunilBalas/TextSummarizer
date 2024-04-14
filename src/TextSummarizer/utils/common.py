eaa6ad0458b8ec0fc02cb1db0d2b01820bf3ca00
SunilBalas <balassunil606@gmail.com>
2024-04-14 11:42:57 +0530

    fixed the file size issue in common.py file

--- src/TextSummarizer/utils/common.py
+++ src/TextSummarizer/utils/common.py
@@ -57,5 +57,5 @@ def get_size(path: Path) -> str:
         str: size in KB
     """
     
-    size_in_kb = round(os.path.getsize(path/1024))
+    size_in_kb = round(os.path.getsize(path)/1024)
     return f"~ {size_in_kb} KB"
