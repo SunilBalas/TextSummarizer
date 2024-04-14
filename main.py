696c9b7a7fa7cca46244f3dbf007f24f7ca02faf
SunilBalas <balassunil606@gmail.com>
2024-04-14 10:34:32 +0530

    Data Ingestion modular coding done

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from TextSummarizer.pipeline.data_ingestion import DataIngestionTrainingPipeline
 from TextSummarizer.logging import logger
 
-logger.info("Welcome to our custom log...!")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>> Stage: {STAGE_NAME} Started <<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> Stage: {STAGE_NAME} Completed <<<<<")
+except Exception as ex:
+    logger.exception(ex)
+    raise ex
